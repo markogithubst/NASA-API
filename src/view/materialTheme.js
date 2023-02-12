ab29b12b816d0d7ea04c5a7a98c7922f16e91856
markogithubst <marko.vukusic2@gmail.com>
2023-02-12 19:05:45 +0100

    exported theme from Data.js

---
+++ src/view/materialTheme.js
@@ -0,0 +1,27 @@
+export const theme = {
+    root: {
+      padding: '40px',
+      maxWidth: '100%',
+      backgroundColor: 'grey',
+      border: 'solid 1px'
+    },
+    image: {
+      width: '60%',
+      height: '60%',
+      objectFit: 'fill'
+    },
+    title: {
+      textAlign: 'center',
+      margin: '20px 0',
+      fontSize: '4em',
+      fontFamily: 'Garamond'
+    },
+    text: {
+      textAlign: 'justify',
+      fontSize: '2em',
+      fontFamily: 'Georgia',
+      maxWidth: '100%',
+      padding: '20px'
+    }
+  };
+  
