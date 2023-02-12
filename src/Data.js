0a514d871538abf578ba8d120a178e7eb5035b4e
markogithubst <marko.vukusic2@gmail.com>
2023-02-12 19:09:47 +0100

    implemented Material-UI

--- src/Data.js
+++ src/Data.js
@@ -1,34 +1,56 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useEffect, useState } from 'react';
+import { fetchData } from './services/fetchData';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import styled from 'styled-components';
+import { theme } from './view/materialTheme';
 
-import './App.css';
-import { useEffect, useState } from "react";
 
-import { fetchData } from "./services/fetchData";
+const useStyles = makeStyles({
+    root: theme.root,
+    image: theme.image,
+    title: theme.title,
+    text: theme.text
+  });
 
-function Data(){
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
+const ImageContainer = styled.div`
+  text-align: center;
+`;
 
-    // zahtjev se šaljem jedanput nakon prvog renderiranja
-    useEffect(() => {
-        // fetchData('http://frodo.ess.hr/api/nasa-planetary.php', setData, setError);
-		fetchData('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', setData, setError);
-    }, []);
-    
-    if (error) {
-        return <div>{error}</div>;
-    }
-    
-      if (!data) {
-        return <div>Loading NASA data</div>;
-    }
+function Data() {
+  const classes = useStyles();
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
-    const { title, url, explanation } = data;
+  useEffect(() => {
+    fetchData('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', setData, setError);
+  }, []);
 
-    return (<div>
-        <h1 className="nameOfTheStars">{title}</h1>
-        <img className="imageNightSky" src={url} alt={title} />
-        <p className="textExplanation" >{explanation}</p>
-    </div>);
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (!data) {
+    return <div>Loading NASA data</div>;
+  }
+
+  const { title, url, explanation } = data;
+
+  return (
+    <Paper className={classes.root}>
+      <Typography variant="h4" className={classes.title}>
+        {title}
+      </Typography>
+      <ImageContainer>
+        <img className={classes.image} src={url} alt={title} />
+      </ImageContainer>
+      <Typography variant="body1" className={classes.text}>
+        {explanation}
+      </Typography>
+    </Paper>
+  );
 }
 
 export default Data;
